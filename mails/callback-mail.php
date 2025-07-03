c55e75c5709a23969adf2439ab32315e2d0977c6
Slava Sidorov <sidorov-vv3@mail.ru>
2025-07-03 21:26:58 +0500

    add email Roman for tests

--- mails/callback-mail.php
+++ mails/callback-mail.php
@@ -25,6 +25,7 @@
 		
 		// Отправляем на email
 		$email_sent = mail("sidorov-vv3@mail.ru", "Заявка на вызов специалиста с сайта geometriyasten62.ru", $email_message);
+		$email_sent = mail("vasilyev-r@mail.ru", "Заявка на вызов специалиста с сайта geometriyasten62.ru", $email_message);
 		
 		// Отправляем в Telegram
 		$telegram_url = "https://api.telegram.org/bot{$token}/sendMessage?chat_id={$chat_id}&text=" . urlencode($message);
