48041ab7e8be020020ffbdb1140d081e632aa693
Slava Sidorov <sidorov-vv3@mail.ru>
2025-07-03 21:16:21 +0500

    add yandex metric

--- index.php
+++ index.php
@@ -1829,6 +1829,9 @@
 
 	<script src="<?php echo get_template_directory_uri(); ?>/js/inputmask.min.js"></script>
 	<script src="<?php echo get_template_directory_uri(); ?>/js/telMask.js"></script>
+
+	<script
+		type="text/javascript"> (function (m, e, t, r, i, k, a) { m[i] = m[i] || function () { (m[i].a = m[i].a || []).push(arguments) }; m[i].l = 1 * new Date(); for (var j = 0; j < document.scripts.length; j++) { if (document.scripts[j].src === r) { return; } } k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a) })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym"); ym(103178591, "init", { clickmap: true, trackLinks: true, accurateTrackBounce: true, webvisor: true }); </script>
 </body>
 
 </html>
